4852408b192b68af6a14d3f6f43cf54d7405ad6f
Matthew Steele <matthewsteeleonline@gmail.com>
2020-08-04 23:38:38 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,7 +4,7 @@
 # or whitespace (' ').
 
 class EmailAddressParser
-  attr_accessor :emails
+  attr_accessor :email_addresses
   def initialize(email_addresses)
         @email_addresses = email_addresses
     end
